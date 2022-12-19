92b82ae0f2c601eaa3631c6d021a4cfc312f52bc
V-Blaze <valentine6586@gmail.com>
2022-12-19 15:34:31 +0100

    Add basic Jsx HTML markup for the profile Page COmponent

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import './Profile.css'
 
 const Profile = () => (
-  <div>Profile</div>
+  <>
+    <div className="profile-main-container">
+      <div className="profile-missions-section">
+        <h3 className="mission-lists">My Missions</h3>
+      </div>
+      <div className="profile-rockets-section">
+        <h3 className="rockets-list">My Rockets</h3>
+      </div>
+    </div>
+  </>
 );
 
 export default Profile;
