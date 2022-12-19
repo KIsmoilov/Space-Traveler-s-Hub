237f96766ea30152aea291d41b621cc6b2594f3c
V-Blaze <valentine6586@gmail.com>
2022-12-19 16:07:57 +0100

    Add proper styling to the profile page component

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import './Profile.css'
+// Components
+import MissionLists from '../components/MissionLists';
+import RocketLists from '../components/RocketLists';
+
+// Stylesheet
+import './Profile.css';
 
 const Profile = () => (
   <>
     <div className="profile-main-container">
       <div className="profile-missions-section">
         <h3 className="mission-lists">My Missions</h3>
+        <MissionLists />
       </div>
       <div className="profile-rockets-section">
         <h3 className="rockets-list">My Rockets</h3>
+        <RocketLists />
       </div>
     </div>
   </>
