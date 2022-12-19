a1cd71fecf4e279108987db1407ebb2fd093a339
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2022-12-19 19:12:20 +0500

    Create webpage header with empty navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 // pages
 import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
 import Profile from './pages/Profile';
+import Navbar from './components/Navbar';
+
 // Stylesheet
 import './App.css';
 
 function App() {
   return (
     <>
-      <Rockets />
-      <Missions />
-      <Profile />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
   );
 }
