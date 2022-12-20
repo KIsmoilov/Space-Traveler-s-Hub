f868542d408daabab87aff770e1cade1243f66a1
V-Blaze <valentine6586@gmail.com>
2022-12-20 10:07:25 +0100

    Add action types using the docks pattern

--- src/Redux/Rockets/Rockets.js
+++ src/Redux/Rockets/Rockets.js
@@ -1,7 +1,18 @@
+const GET_ALL_ROCKETS = 'Rockets/Rockets/GET_ALL_ROCKETS';
+const RESERVE_ROCKET = 'Rockets/Rockets/RESERVE_ROCKET';
+const CANCEL_RESERVATION = 'Rockets/Rockets/CANCEL_RESERVATION';
+
 const initialState = [];
 
 export default function reducer(state = initialState, action = {}) {
-  // eslint-disable-next-line
-  console.log(state, action);
-  return state;
+  switch (action.type) {
+    case GET_ALL_ROCKETS:
+      return state;
+    case RESERVE_ROCKET:
+      return state;
+    case CANCEL_RESERVATION:
+      return state;
+    default:
+      return state;
+  }
 }
