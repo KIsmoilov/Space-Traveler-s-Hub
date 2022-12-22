a0a61c4ad5b7bc792cc2f039c58a8ae8736b3f87
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2022-12-22 15:22:23 +0500

    Display the list of joined missions on profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -9,14 +9,25 @@ import './Profile.css';
 
 const Profile = () => {
   const rockets = useSelector((state) => state.Rockets);
+  const missions = useSelector((state) => state.Missions);
   const reservedRockets = rockets.filter((item) => item.reserved === true);
+  const joinedMissions = missions.filter((mission) => mission.joined === true);
 
   return (
     <>
       <div className="profile-main-container">
         <div className="profile-missions-section">
           <h3 className="mission-lists">My Missions</h3>
-          <MissionLists />
+          {joinedMissions.length ? (
+            joinedMissions.map((mission) => (
+              <MissionLists
+                key={mission.mission_id}
+                mission={mission}
+              />
+            ))
+          ) : (
+            <span className="no-reserved">No Joined Missions!!!</span>
+          )}
         </div>
         <div className="profile-rockets-section">
           <h3 className="rockets-list">My Rockets</h3>
